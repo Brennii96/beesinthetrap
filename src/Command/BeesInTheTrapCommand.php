cc4af0656a34b27607848bf425f88b4aa361ecc5
brendan <16559757+Brennii96@users.noreply.github.com>
2025-04-19 15:15:16 +0100

    start work on the command, auto mode first

--- src/Command/BeesInTheTrapCommand.php
+++ src/Command/BeesInTheTrapCommand.php
@@ -2,8 +2,14 @@
 
 namespace App\Command;
 
+use App\Entity\Hive;
+use App\Entity\Player;
+use App\Factory\BeeFactory;
+use App\Game\AttackService;
+use App\Game\GameEngine;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class BeesInTheTrapCommand extends Command
@@ -11,12 +17,43 @@ class BeesInTheTrapCommand extends Command
     protected function configure(): void
     {
         $this->setName('beesinthetrap:play')
+            ->addOption(
+                'auto',
+                null,
+                InputOption::VALUE_NONE,
+                'Auto play without user input... but where\'s the fun in that?'
+            )
             ->setDescription('Play Bees in the Trap');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $output->writeln('Bees in the Trap');
+        $hive = new Hive(new BeeFactory());
+        $player = new Player();
+        $attackService = new AttackService();
+        $engine = new GameEngine($hive, $player, $attackService);
+
+        $autoPlay = $input->getOption('auto');
+        $output->writeln($autoPlay ? 'Starting auto-play mode...' : '');
+
+        while (!$engine->isOver()) {
+            $output->writeln($engine->playerTurn());
+            $beesTurn = $engine->beesTurn();
+            if ($beesTurn !== null) {
+                $output->writeln("Hit");
+            } else {
+                $output->writeln("All bees are dead! You won!");
+                break;
+            }
+
+            $output->writeln('Your HP: ' . $player->getHp());
+            $output->writeln('Bees alive: ' . count($hive->getAliveBees()));
+        }
+        $output->writeln('--- GAME OVER ---');
+        $output->writeln('Hits dealt: ' . $player->getHits());
+        $output->writeln('Stings taken: ' . $player->getStings());
+        $output->writeln($player->isAlive() ? 'You survived!' : 'You died!');
+
         return Command::SUCCESS;
     }
 }
